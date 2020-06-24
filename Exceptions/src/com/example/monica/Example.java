22d61f8966b44d3ed9e7fcd162d518e219a9553d
Monica Tamaru Jones <mntamaru@gmail.com>
2020-06-24 14:44:27 -0700

    finish up exceptions lesson

--- Exceptions/src/com/example/monica/Example.java
+++ Exceptions/src/com/example/monica/Example.java
@@ -7,31 +7,33 @@ import java.util.Scanner;
 public class Example {
 
     public static void main(String[] args) {
-        int result = divide();
-        System.out.println(result);
+        try {
+            int result = divide();
+            System.out.println(result);
+        }
+        catch(ArithmeticException e) {
+            System.out.println(e.toString());
+            System.out.println("Unable to perform division");
+        }
 
     }
 
     private static int divide() {
         int x, y;
 
-
         try {
             x = getInt();
             y= getInt();
+            System.out.println("x is " + x + " and y is " + y);
+            return x/y;
+
         }
         catch (NoSuchElementException e) {
             throw new ArithmeticException("No suitable input");
         }
-
-        System.out.println("x is " + x + " and y is " + y);
-        try{
-            return x / y;
-        }
         catch (ArithmeticException e) {
             throw new ArithmeticException("Attempt to divide by zero");
         }
-
     }
 
     private static int getInt() {
