0becd34d31dc1687f6751903fae04944dcb5d734
Monica Tamaru Jones <mntamaru@gmail.com>
2020-06-24 14:38:53 -0700

    throw exception example

--- Exceptions/src/com/example/monica/Example.java
+++ Exceptions/src/com/example/monica/Example.java
@@ -1,4 +1,52 @@
 package com.example.monica;
 
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class Example {
+
+    public static void main(String[] args) {
+        int result = divide();
+        System.out.println(result);
+
+    }
+
+    private static int divide() {
+        int x, y;
+
+
+        try {
+            x = getInt();
+            y= getInt();
+        }
+        catch (NoSuchElementException e) {
+            throw new ArithmeticException("No suitable input");
+        }
+
+        System.out.println("x is " + x + " and y is " + y);
+        try{
+            return x / y;
+        }
+        catch (ArithmeticException e) {
+            throw new ArithmeticException("Attempt to divide by zero");
+        }
+
+    }
+
+    private static int getInt() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter an integer: ");
+        while(true) {
+            try {
+                return s.nextInt();
+            }
+            catch(InputMismatchException e){
+                // keep trying to get valid input
+                s.nextLine();
+                System.out.println("Please enter a valid integer: ");
+            }
+        }
+
+    }
 }
