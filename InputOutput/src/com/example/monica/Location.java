341021975ea75fd164ff836f52b426aa92c8bd2b
Monica Tamaru Jones <mntamaru@gmail.com>
2020-06-30 10:54:14 -0700

    add Serializable to Location class

--- InputOutput/src/com/example/monica/Location.java
+++ InputOutput/src/com/example/monica/Location.java
@@ -1,14 +1,17 @@
 package com.example.monica;
 
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class Location {
+public class Location implements Serializable {
     // lets everyone know, don't change these - no setters, as well
     private final int locationID;
     private final String description;
     private final Map<String, Integer> exits;
+
+    private long serialVersionUID = 1L;
 
     public Location(int locationID, String description, Map<String, Integer> exits) {
         this.locationID = locationID;
