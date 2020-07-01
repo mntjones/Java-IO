ea549a4a08bc5e4cc620270720f8a519de3c76a5
Monica Tamaru Jones <mntamaru@gmail.com>
2020-07-01 16:45:41 -0700

    use File.write to write to file

--- JavaNio/src/com/example/monica/Main.java
+++ JavaNio/src/com/example/monica/Main.java
@@ -1,8 +1,31 @@
 package com.example.monica;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    try {
+//            FileInputStream file = new FileInputStream("data.txt");
+//            FileChannel channel = file.getChannel();
+            Path dataPath = FileSystems.getDefault().getPath("data.txt");
+            // StandardOpenOption appends to file - default is to create new or wipe out existing file
+            Files.write(dataPath, "\nLine 5".getBytes("UTF-8"), StandardOpenOption.APPEND);
+            List<String> lines = Files.readAllLines(dataPath);
+
+            for (String line : lines) {
+                System.out.println(line);
+            }
+        }
+	    catch(IOException e) {
+	        e.printStackTrace();
+        }
     }
 }
