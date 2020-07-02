f1e1817be7b1be25d4a27f1dc24d899138c69900
Monica Tamaru Jones <mntamaru@gmail.com>
2020-07-02 11:21:57 -0700

    practice with buffer position

--- JavaNio/src/com/example/monica/Main.java
+++ JavaNio/src/com/example/monica/Main.java
@@ -3,6 +3,7 @@ package com.example.monica;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.FileSystems;
@@ -34,6 +35,55 @@ public class Main {
             intBuffer.flip();
             numBytes = binChannel.write(intBuffer);
             System.out.println("numBytes written was: " + numBytes);
+
+            // uses java.nio
+
+            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
+            FileChannel channel = ra.getChannel();
+            outputBytes[0] ='a';
+            outputBytes[1] = 'b';
+            buffer.flip();
+            long numBytesRead = channel.read(buffer);
+            if (buffer.hasArray()) {
+                System.out.println("byte buffer = " + new String(buffer.array()));
+            }
+
+            intBuffer.flip();
+            numBytesRead = channel.read(intBuffer);
+
+            // Absolute read for intBuffer.getInt();
+            System.out.println(intBuffer.getInt(0));
+
+            intBuffer.flip();
+            numBytes = channel.read(intBuffer);
+            System.out.println(intBuffer.getInt(0));
+
+            // Relative read for intBuffer.getInt()
+//            intBuffer.flip();
+//            System.out.println(intBuffer.getInt());
+//
+//            intBuffer.flip();
+//            numBytesRead = channel.read(intBuffer);
+//
+//            intBuffer.flip();
+//            System.out.println(intBuffer.getInt());
+
+            channel.close();
+            ra.close();
+            //System.out.println("output bytes = " + new String(outputBytes));
+
+
+    // uses java.io
+//            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
+//            byte[] b = new byte[outputBytes.length];
+//            ra.read(b);
+//            System.out.println(new String(b));
+//
+//            long int1 = ra.readInt();
+//            long int2 = ra.readInt();
+//
+//            System.out.println(int1);
+//            System.out.println(int2);
 
             //write to data.txt file
 //            FileInputStream file = new FileInputStream("data.txt");
