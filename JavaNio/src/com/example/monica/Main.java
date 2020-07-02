14f88210ed1b67677eb786f3273ecf36b9825c01
Monica Tamaru Jones <mntamaru@gmail.com>
2020-07-01 17:12:10 -0700

    practice with ByteBuffer

--- JavaNio/src/com/example/monica/Main.java
+++ JavaNio/src/com/example/monica/Main.java
@@ -1,7 +1,9 @@
 package com.example.monica;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
@@ -12,17 +14,38 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-	    try {
+	    try (FileOutputStream binFile = new FileOutputStream("data.dat");
+             FileChannel binChannel = binFile.getChannel()){
+
+            byte[] outputBytes = "Hello World!".getBytes();
+            ByteBuffer buffer = ByteBuffer.wrap(outputBytes);
+            int numBytes = binChannel.write(buffer);
+            System.out.println("numBytes written was: " + numBytes);
+
+            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
+            intBuffer.putInt(245);
+            // resets the buffer back to zero
+            intBuffer.flip();
+            numBytes = binChannel.write(intBuffer);
+            System.out.println("numBytes written was: " + numBytes);
+
+            intBuffer.flip();
+            intBuffer.putInt(-98765);
+            intBuffer.flip();
+            numBytes = binChannel.write(intBuffer);
+            System.out.println("numBytes written was: " + numBytes);
+
+            //write to data.txt file
 //            FileInputStream file = new FileInputStream("data.txt");
 //            FileChannel channel = file.getChannel();
-            Path dataPath = FileSystems.getDefault().getPath("data.txt");
-            // StandardOpenOption appends to file - default is to create new or wipe out existing file
-            Files.write(dataPath, "\nLine 5".getBytes("UTF-8"), StandardOpenOption.APPEND);
-            List<String> lines = Files.readAllLines(dataPath);
-
-            for (String line : lines) {
-                System.out.println(line);
-            }
+//            Path dataPath = FileSystems.getDefault().getPath("data.txt");
+//            // StandardOpenOption appends to file - default is to create new or wipe out existing file
+//            Files.write(dataPath, "\nLine 5".getBytes("UTF-8"), StandardOpenOption.APPEND);
+//            List<String> lines = Files.readAllLines(dataPath);
+//
+//            for (String line : lines) {
+//                System.out.println(line);
+//            }
         }
 	    catch(IOException e) {
 	        e.printStackTrace();
