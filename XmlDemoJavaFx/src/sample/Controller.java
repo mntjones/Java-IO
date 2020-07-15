cf635443675c1f1f1b02b3dbe4ac9d0ec6b55c9b
Monica Tamaru Jones <mntamaru@gmail.com>
2020-07-15 11:51:07 -0700

    html for buttons

--- XmlDemoJavaFx/src/sample/Controller.java
+++ XmlDemoJavaFx/src/sample/Controller.java
@@ -1,4 +1,18 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+
 public class Controller {
+
+//    @FXML
+//    private ListView<Telemetry> telemetryListView;
+//    @FXML
+//    private TextArea telemetryText;
+//
+//
+//    public void initialize() {
+//
+//    }
 }
--- XmlDemoJavaFx/src/sample/Telemetry.java
+++ XmlDemoJavaFx/src/sample/Telemetry.java
@@ -13,23 +13,21 @@ import java.io.IOException;
 public class Telemetry {
 
     private static Telemetry instance = new Telemetry();
-    private static String filename = "src/TelemetryData.xml";
+    // creates a constructor of file class and parsing XML file
+    private DocumentBuilder db = null;
+    private File file = new File("src/TelemetryData.xml");
 
     public static Telemetry getInstance() {
         return instance;
     }
 
     public void loadTelemetryXml() throws IOException {
-        // creates a constructor of file class and parsing XML file
-        try {
-            // creates a constructor of file class and parsing XML file
-            File file = new File(filename);
 
+        try {
             // an instance of factory that gives a document builder
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 
             // an instance of builder to parse the specified xml file
-            DocumentBuilder db = null;
             try {
                 db = dbf.newDocumentBuilder();
             } catch (ParserConfigurationException parserConfigurationException) {
@@ -65,6 +63,36 @@ public class Telemetry {
             }
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    public void readTelemetry() {
+        Document doc = null;
+        try {
+            doc = db.parse(file);
+        } catch (SAXException saxException) {
+            saxException.printStackTrace();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+
+        doc.getDocumentElement().normalize();
+        System.out.println("Root Element: " + doc.getDocumentElement().getNodeName());
+        NodeList nodeList = doc.getElementsByTagName("*");
+
+        //nodelist is not iterable - use for loop to read file - print out contents
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node = nodeList.item(i);
+
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                // THIS WORKS
+                System.out.println(node.getNodeName() + " : " + node.getChildNodes().item(0).getNodeValue());
+
+                // BAD check works
+//                    if (node.getChildNodes().item(0).getNodeValue().contains("bad")) {
+//                        System.out.println("!!!!!!!!!BAD FILES!!!!!!!!!!");
+//                    }
+            }
         }
     }
 }
