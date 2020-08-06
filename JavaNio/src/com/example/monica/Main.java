d04701f0b9e65ae920cf7621f90b66f496ba34c4
Monica Tamaru Jones <mntamaru@gmail.com>
2020-08-06 13:27:26 -0700

    file copy

--- JavaNio/src/com/example/monica/Main.java
+++ JavaNio/src/com/example/monica/Main.java
@@ -52,20 +52,84 @@ public class Main {
             RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
             FileChannel channel = ra.getChannel();
 
-            ByteBuffer readBuffer = ByteBuffer.allocate(100);
+            // read ints backwards
+            ByteBuffer readBuffer = ByteBuffer.allocate(Integer.BYTES);
+            channel.position(int3pos);
             channel.read(readBuffer);
-            // switch from reading to writing, so need to flip
             readBuffer.flip();
-            byte[] inputString = new byte[outputBytes.length];
-            readBuffer.get(inputString);
-            System.out.println("input String = " + new String(inputString));
-            System.out.println("int1 = " + readBuffer.getInt());
-            System.out.println("int2 = " + readBuffer.getInt());
-
-            byte[] inputString2 = new byte[outputBytes2.length];
-            readBuffer.get(inputString2);
-            System.out.println("input String2 = " + new String(inputString2));
             System.out.println("int3 = " + readBuffer.getInt());
+
+            readBuffer.flip();
+            channel.position(int2pos);
+            channel.read(readBuffer);
+            readBuffer.flip();
+            System.out.println("int2= " + readBuffer.getInt());
+
+            readBuffer.flip();
+            channel.position(int1pos);
+            channel.read(readBuffer);
+            readBuffer.flip();
+            System.out.println("int1= " + readBuffer.getInt());
+
+            RandomAccessFile copyFile = new RandomAccessFile("datacopy.dat", "rw");
+            FileChannel copyChannel = copyFile.getChannel();
+            channel.position(0);
+            long numTransferred = copyChannel.transferFrom(channel, 0, channel.size());
+            System.out.println("Num copied: " + numTransferred);
+
+            channel.close();
+            ra.close();
+            copyChannel.close();
+
+            //
+
+            byte[] outputString = "Hello, Monica!".getBytes();
+            long str1Pos = 0;
+            long newInt1Pos = outputString.length;
+            long newInt2Pos = newInt1Pos + Integer.BYTES;
+
+            byte[] outputString2 = "You are pretty".getBytes();
+            long str2Pos = newInt2Pos + Integer.BYTES;
+            long newInt3Pos = str2Pos + outputString2.length;
+
+            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
+            intBuffer.putInt(23);
+            intBuffer.flip();
+            binChannel.position(newInt1Pos);
+            binChannel.write(intBuffer);
+
+            intBuffer.flip();
+            intBuffer.putInt(-9381);
+            intBuffer.flip();
+            binChannel.position(newInt2Pos);
+            binChannel.write(intBuffer);
+
+            intBuffer.flip();
+            intBuffer.putInt(81416);
+            intBuffer.flip();
+            binChannel.position(newInt3Pos);
+            binChannel.write(intBuffer);
+
+            binChannel.position(str1Pos);
+            binChannel.write(ByteBuffer.wrap(outputString));
+            binChannel.position(str2Pos);
+            binChannel.write(ByteBuffer.wrap(outputString2));
+
+
+//            ByteBuffer readBuffer = ByteBuffer.allocate(100);
+//            channel.read(readBuffer);
+//            // switch from reading to writing, so need to flip
+//            readBuffer.flip();
+//            byte[] inputString = new byte[outputBytes.length];
+//            readBuffer.get(inputString);
+//            System.out.println("input String = " + new String(inputString));
+//            System.out.println("int1 = " + readBuffer.getInt());
+//            System.out.println("int2 = " + readBuffer.getInt());
+//
+//            byte[] inputString2 = new byte[outputBytes2.length];
+//            readBuffer.get(inputString2);
+//            System.out.println("input String2 = " + new String(inputString2));
+//            System.out.println("int3 = " + readBuffer.getInt());
 
         }
 	    catch(IOException e) {
