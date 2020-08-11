ebfe24c85f87dbd68b6f0dc81ffc5a9bed634cfe
Monica Tamaru Jones <mntamaru@gmail.com>
2020-08-11 16:04:44 -0700

    basic read

--- Paths/src/com/example/monica/Main.java
+++ Paths/src/com/example/monica/Main.java
@@ -1,8 +1,26 @@
 package com.example.monica;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+    }
+
+    private static void printFile(Path path) {
+        try(BufferedReader fileReader = Files.newBufferedReader(path)) {
+            String line;
+            while((line = fileReader.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
