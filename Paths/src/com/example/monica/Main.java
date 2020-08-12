e90019bb1bd12888ecfe50216056199cd540ff36
Monica Tamaru Jones <mntamaru@gmail.com>
2020-08-12 15:01:52 -0700

    looking at file paths on the computer

--- Paths/src/com/example/monica/Main.java
+++ Paths/src/com/example/monica/Main.java
@@ -2,13 +2,33 @@ package com.example.monica;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
+        System.out.println(path);
+        printFile(path);
+
+        System.out.println("\n ----------- \n");
+
+        //Path filePath = FileSystems.getDefault().getPath("files","SubdirectoryFile.txt");
+        Path filePath = Paths.get(".","files", "SubdirectoryFile.txt");
+
+        printFile(filePath);
+
+        System.out.println("\n ----------- \n");
+
+        filePath = Paths.get("/Users/monica/Google Drive/Java-IO/OutThere.txt");
+        printFile(filePath);
+
+        filePath = Paths.get(".");
+        System.out.println(filePath.toAbsolutePath());
 
     }
 
